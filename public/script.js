660de44c8661185262312438afaea19af410ed20
Geoff Gorham <geogor37@gmail.com>
2019-03-28 16:56:46 -0600

    Attempted to set up the get and post endpoints for wishlists

--- public/script.js
+++ public/script.js
@@ -6,11 +6,30 @@ var app = new Vue({
     selectedList: {},
     newItemName: '',
   },
+  created() {
+    this.getLists();
+  },
   methods: {
-    createList() {
-      if (this.newListName.trim().length > 0) {
-        this.lists.push({ name: this.newListName, items: [] });
-        this.newListName = '';
+    async getLists() {
+      try {
+        let response = await axios.get("/api/wishlists");
+        this.lists = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async createList() {
+      try {
+        if (this.newListName.trim().length > 0) {
+          let listResponse = axios.post("/api/wishlists", {
+            title: this.newListName
+          });
+          this.newListName = '';
+          this.lists.add(await listResponse);
+        }
+      } catch (error) {
+        console.log(error);
       }
     },
     deleteList(list) {
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static('public'));
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/wishlist', {
+  useNewUrlParser: true
+});
+
+// Create a schema for wishlists
+const wishlistSchema = new mongoose.Schema({
+  title: String,
+  items: [{ type: String }]
+});
+
+// Create a model for wishlists.
+const Wishlist = mongoose.model('Wishlist', wishlistSchema);
+
+// Get all the wishlists that have been created.
+app.get('/api/wishlists', async (req, res) => {
+  try {
+    let wishlists = await Wishlist.find();
+    res.send(wishlists);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Create a new wishlist; takes a title for the list.
+app.post('/api/wishlists', async (req, res) => {
+  const wishlist = new Wishlist({
+    title: req.body.title,
+    items: []
+  });
+  try {
+    await wishlist.save();
+    res.send(wishlist);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
